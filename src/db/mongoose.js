1eef5089600595e6335eb52e6f533ccc6503864f
revital-m <revital13m@gmail.com>
2022-01-21 17:15:16 +0200

    change the first arg in mongoose.connect to a string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
-require('dotenv').config({ path: 'ENV_FILENAME' });
+// const path = require('path');
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL, {}, (error, client) => {
+mongoose.connect(`${process.env.MONGO_URL}`, {}, (error, client) => {
     if (error) {
         console.log(error.message);
     }
