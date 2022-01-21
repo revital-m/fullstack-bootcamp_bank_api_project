9403f3f7b7879d2f853376b72b96c842d5d3f592
revital-m <revital13m@gmail.com>
2022-01-21 17:09:24 +0200

    add env filename

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'ENV_FILENAME' });
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL, {}, (error, client) => {
